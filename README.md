2b4d49d2a625df4690c26157c8997df41afa8896
rmotta.net:~# <114815819+rmottanet@users.noreply.github.com>
2024-09-16 14:50:20 -0300

    Refactor: Improved readability of data consolidator (#7)
    * refactor(data-consolidator): Improved code readability
    
    * Update README.md
    
    * Update LEIAME.md
    
    ---------
    
    Co-authored-by: Rodrigo Motta <rmotta.info@gmail.com>

--- README.md
+++ README.md
@@ -16,20 +16,21 @@ To utilize exRate Hunter, follow these instructions:
 2. Install the dependencies listed in the `requirements.txt` file.
 3. Configure the environment variables in the `.env` file by renaming the `.env-example` file.
 4. Execute the `main.py` file to start the FastAPI server.
-	```bash
-	uvicorn app.main:app --host 0.0.0.0 --port 8000
-	```
+    ```bash
+    uvicorn app.main:app --host 0.0.0.0 --port 8000
+    ```
 
 For more information about the CoinSnark project, please refer to the documentation: [CoinSnark GitBook](https://rmottanet.gitbook.io/coinsnark).
+
+---
 
 Thank you for exploring exRate Hunter. If you have any questions or suggestions regarding the project, I am happy to listen. Please feel free to get in touch.
 
 <br />
 <br />
-<p align="center">
-<a href="https://gitlab.com/rmottanet"><img src="https://img.shields.io/badge/Gitlab--_.svg?style=social&logo=gitlab" alt="GitLab"></a>
-<a href="https://github.com/rmottanet"><img src="https://img.shields.io/badge/Github--_.svg?style=social&logo=github" alt="GitHub"></a>
-<a href="https://instagram.com/rmottanet/"><img src="https://img.shields.io/badge/Instagram--_.svg?style=social&logo=instagram" alt="Instagram"></a>
-<a href="https://www.linkedin.com/in/rmottanet/"><img src="https://img.shields.io/badge/Linkedin--_.svg?style=social&logo=linkedin" alt="Linkedin"></a>
-</p>
-<br />
+<div align="center">
+  <a href="https://bitbucket.org/rmottalabs/"><img alt="Static Badge" src="https://img.shields.io/badge/-Bitbucket?style=social&logo=bitbucket&logoSize=auto&label=Bitbucket&link=https%3A%2F%2Fbitbucket.org%2Frmottalabs%2Fworkspace%2Foverview%2F"></a>
+  <a href="https://gitlab.com/rmottanet"><img alt="Static Badge" src="https://img.shields.io/badge/-Gitlab?style=social&logo=gitlab&logoSize=auto&label=Gitlab&link=https%3A%2F%2Fgitlab.com%2Frmottanet"></a>
+  <a href="https://github.com/rmottanet"><img alt="Static Badge" src="https://img.shields.io/badge/-Github?style=social&logo=github&logoSize=auto&label=Github&link=https%3A%2F%2Fgithub.com%2Frmottanet"></a>
+  <a href="https://hub.docker.com/"><img alt="Static Badge" src="https://img.shields.io/badge/-DockerHub?style=social&logo=docker&logoSize=auto&label=DockerHub&link=https%3A%2F%2Fhub.docker.com%2Fu%2Frmottanet"></a>
+</div>
--- app/data/consolidation.py
+++ app/data/consolidation.py
@@ -2,33 +2,59 @@ import logging
 from typing import Dict
 from app.utils.time_control import TimeCtrl
 
+
 class DataConsolidator:
+    """
+    This class consolidates data from multiple external services.
+
+    It takes the data from three services as dictionaries and combines them
+    into a single dictionary with priority given to the services in the order
+    they are provided.  Duplicate currencies will be populated with the value
+    from the last service that provided a value for that currency.
+
+    **Attributes:**
+
+    * services (list): A list containing dictionaries of data from each service.
+
+    **Methods:**
+
+    * consolidate() -> Dict[str, float]: Consolidates data from all services
+        and returns a single dictionary containing the combined results.
+    """
+
     def __init__(self, service_1_data: Dict[str, float], service_2_data: Dict[str, float], service_3_data: Dict[str, float]):
-        self.service_1_data = service_1_data
-        self.service_2_data = service_2_data
-        self.service_3_data = service_3_data
+        """
+        Initializes the DataConsolidator with data from three services.
+
+        **Args:**
+
+        * service_1_data (Dict[str, float]): Dictionary containing data from service 1.
+        * service_2_data (Dict[str, float]): Dictionary containing data from service 2.
+        * service_3_data (Dict[str, float]): Dictionary containing data from service 3.
+        """
+        self.services = [service_1_data, service_2_data, service_3_data]
 
     def consolidate(self) -> Dict[str, float]:
-        consolidated_data = {}
+        """
+        Consolidates data from all services and returns a single dictionary.
 
-        # Função para verificar se uma moeda tem uma resposta não vazia em algum dos serviços
-        def has_non_empty_response(currency):
-            return any(data.get(currency) for data in [self.service_1_data, self.service_2_data, self.service_3_data])
+        This method iterates over the data from each service and combines them
+        into a single dictionary.  If a currency exists in multiple services,
+        the value from the last service encountered will be used.
 
-        # Inserir a chave 'data_base' no início do dicionário consolidado
-        consolidated_data['DATA_BASE'] = TimeCtrl.get_current_utc_time()
-        
-        # Atualiza os dados consolidados por prioridade
-        for currency in set(self.service_1_data.keys()) | set(self.service_2_data.keys()) | set(self.service_3_data.keys()):
-            if has_non_empty_response(currency):
-                if currency in self.service_1_data:
-                    consolidated_data[currency] = self.service_1_data[currency]
-                elif currency in self.service_2_data:
-                    consolidated_data[currency] = self.service_2_data[currency]
-                elif currency in self.service_3_data:
-                    consolidated_data[currency] = self.service_3_data[currency]
-                else:
-                    logging.warning(f"Nenhuma resposta válida encontrada para {currency}")
+        **Returns:**
+
+        * Dict[str, float]: A dictionary containing the consolidated data.
+            The dictionary key is the currency code and the value is the
+            corresponding exchange rate.
+        """
+
+        consolidated_data = {'DATA_BASE': TimeCtrl.get_current_utc_time()}
+
+        for service_data in self.services:
+            if isinstance(service_data, dict):
+                for currency, value in service_data.items():
+                    if currency not in consolidated_data:
+                        consolidated_data[currency] = value
 
         return consolidated_data
-
